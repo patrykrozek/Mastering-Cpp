b915cb3226cce962f84a9376ae5249bd8796ca44
parozek@student.agh.edu.pl <parozek@student.agh.edu.pl>
2024-11-15 14:58:32 +0100

    My version of solving the Leibniz series

---
+++ Basics/Leibniz_series/Leibniz_series.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    double P = 0, licznik = 1;
+    cout << "Podaj n" << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i += 2) {
+        P += licznik / i;
+        licznik *= -1;
+    }
+    P = 4 * P;
+    cout << P << endl;
+}
