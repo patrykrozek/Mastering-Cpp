634753b0333cd2ab5eb0036ba1d8d94115f82ad1
parozek@student.agh.edu.pl <parozek@student.agh.edu.pl>
2024-12-03 11:04:53 +0100

    2d arrays

--- Basics/2d_arrays/2d_arrays.cpp
+++ Basics/2d_arrays/2d_arrays.cpp
@@ -3,10 +3,12 @@ Tasks to do:
 - creat 2d array with random no from defined scope - DONE
 - calculate trace of the matrix - DONE
 - calculate min value and show it's position DONE
-- transposition - In progress
+- transposition - DONE
+- reshape - DONE
+- delete x row and column from the matrix -DONE
+
+============== UNDONE ==============
 - calculate the determinant of the matrix
-- reshape
-- delete x row and column from the matrix
 */
 
 
@@ -15,6 +17,8 @@ void matrix_printer(int**, int, int);
 int matrix_trace(int**, int, int);
 void min_value(int**, int, int);
 int** matrix_trans(int**, int, int);
+int** matrix_reshape(int**, int, int,int,int);
+int** matrix_del_row_col(int**, int, int,int,int);
 
 
 
@@ -24,7 +28,7 @@ using namespace std;
 
 
 int main() {
-	int r, c, top_limit = 50,down_limit=10;
+	int r, c, top_limit = 50,down_limit=10,nr,nc,dr,dc;
 	
 	cout << "Enter the R x C size of the matrix: ";
 	cin >> r>>c;
@@ -34,6 +38,22 @@ int main() {
 	min_value(matrix, r, c);
 	int** c_matrix = matrix_trans(matrix, r, c);
 	matrix_printer(c_matrix, c, r);
+	cout << "Enter newR and newC size of the matrix"<<endl;
+	cin >> nr >> nc;
+	if (r * c != nr * nc) {
+		cout << "Wrong sum of items in new matrix";
+	}
+	//else if (r==c) {
+	//	cout << "Determinant of the matrix: " << matrix_del_row_col(matrix, r, c);
+	//}
+	else {
+	int** r_matrix = matrix_reshape(matrix, r, c,nr,nc);
+	matrix_printer(r_matrix, nr, nc);
+	}
+	cout << "Enter no. of row and column, you want to delete"<<endl;
+	cin >> dr >> dc;
+	int** new_matrix = matrix_del_row_col(matrix, r, c,dr,dc);
+	matrix_printer(new_matrix, r-1, c-1);
 }
 
 int** matrix_creator(int r, int c, int top_limit,int down_limit) {
@@ -101,4 +121,46 @@ int** matrix_trans(int** matrix, int c, int r) {
 		}
 	}
 	return c_matrix;
+}
+
+int** matrix_reshape(int** matrix, int r, int c,int nr,int nc) {
+	
+	int** r_matrix = new int* [nr];
+	for (int i = 0; i < nr; ++i) {
+		r_matrix[i] = new int [nc];
+	}
+	for (int i = 0; i < r * c; ++i) {
+		//Maping old values
+		int old_row = i / c;
+		int old_col = i % c;
+		
+		//Maping new values
+		int new_row = i / nc;
+		int new_col = i % nc;
+
+		r_matrix[new_row][new_col] = matrix[old_row][old_col];
+	}
+	return r_matrix;
+}
+
+int** matrix_del_row_col(int** matrix, int r, int c,int dr,int dc) {
+	int** new_matrix = new int* [r-1];
+	for (int i = 0; i < r - 1; ++i) {
+		new_matrix[i] = new int[c-1];
+	}
+
+	int ni = 0;
+	for (int i = 0; i < r; ++i) {
+		if (i == dr)continue;
+		int nj = 0;
+		for (int j = 0; j < c; ++j) {
+			if (j == dc)continue;
+			new_matrix[ni][nj] = matrix[i][j];
+			++nj;
+		}
+		++ni;
+	}
+
+	return new_matrix;
+
 }
